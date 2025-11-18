28efa553db93449183dea4c20f861a92db5a13a1
Betchief Technology Limited <official.betchief@betchief.ng>
2025-11-18 11:14:10 +0100

    implemented live wallet update in the admin/me

--- src/controllers/getCurrentAdminController.js
+++ src/controllers/getCurrentAdminController.js
@@ -1,18 +1,26 @@
 
 import Admin from "../models/Admin.js";
+import Wallet from "../models/wallet.js";
 
 // Get only the current logged-in admin (not all)
 export const getCurrentAdmin = async (req, res) => {
     try {
         // req.admin is attached from your auth middleware
-        const admin = await Admin.findById(req.admin._id).select("apiKeys firstName lastName email _id walletBalance");
+        const admin = await Admin.findById(req.admin._id).select("apiKeys firstName lastName email _id");
         console.log(admin)
         
         if (!admin) {
             return res.status(404).json({ message: "Admin not found" });
         }
+
+        // ✅ Fetch REAL waalet balance
+        const wallet = await Wallet.findOne({ admin: admin._id });
+        const liveBalance = wallet ? wallet.balance : 0
         
-        return res.json(admin); // <--- returns admin object directly
+        return res.json({
+            ...admin.toObject(),
+            walletBalance: liveBalance
+        }); // <--- returns admin object directly
     } catch (err) {
         console.error("Error fetching admin:", err);
         return res.status(500).json({ message: "Error fetching admin", error: err.message });
